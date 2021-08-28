a7140cfdc4067183e98d0aab12cf5a04328ae35f
Harpreet Deol <harpreetdeol@Harpreets-MacBook-Pro.local>
2021-08-28 13:32:18 +0530

    First Commit

---
+++ dist/api/controllers/requests/UpdateCategoryRequest.js
@@ -0,0 +1,29 @@
+"use strict";
+/*
+ * spurtcommerce API
+ * version 3.0.1
+ * Copyright (c) 2019 piccosoft ltd
+ * Author piccosoft ltd <support@piccosoft.com>
+ * Licensed under the MIT license.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UpdateCategoryRequest = void 0;
+const tslib_1 = require("tslib");
+require("reflect-metadata");
+const class_validator_1 = require("class-validator");
+class UpdateCategoryRequest {
+}
+tslib_1.__decorate([
+    class_validator_1.IsNotEmpty(),
+    tslib_1.__metadata("design:type", Number)
+], UpdateCategoryRequest.prototype, "categoryId", void 0);
+tslib_1.__decorate([
+    class_validator_1.IsNotEmpty(),
+    tslib_1.__metadata("design:type", String)
+], UpdateCategoryRequest.prototype, "name", void 0);
+tslib_1.__decorate([
+    class_validator_1.IsNotEmpty(),
+    tslib_1.__metadata("design:type", Number)
+], UpdateCategoryRequest.prototype, "sortOrder", void 0);
+exports.UpdateCategoryRequest = UpdateCategoryRequest;
+//# sourceMappingURL=UpdateCategoryRequest.js.map
